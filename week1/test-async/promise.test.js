a240dcdf6e90ca66c0bc17a1f499ef5f05e99af4
Jim Cramer <remarcmij@gmail.com>
2024-03-16 21:24:27 +0100

    Add async jest examples

---
+++ week1/test-async/promise.test.js
@@ -0,0 +1,23 @@
+function isEven(number) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (number % 2 == 0) {
+        resolve(number);
+      } else {
+        reject(new Error('value is not even'));
+      }
+    }, 1000);
+  });
+}
+
+describe('isEven promise', () => {
+  test('it should return the argument if the number argument is even', () => {
+    expect.assertions(1);
+    return isEven(4).then((data) => expect(data).toBe(4));
+  });
+
+  test('it should return an error if the number argument is not even', () => {
+    expect.assertions(1);
+    return isEven(5).catch((err) => expect(err).toBeInstanceOf(Error));
+  });
+});
