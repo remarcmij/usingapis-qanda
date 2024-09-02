224a5f748162cdd7839597446011c7b6b52e0923
Jim Cramer <remarcmij@gmail.com>
2024-09-02 10:27:11 +0200

    Bug fix

--- week1/catwalk-multi-fun/helpers.js
+++ week1/catwalk-multi-fun/helpers.js
@@ -2,10 +2,10 @@ export function displayResult(fn, thenOrCatch, promiseResult) {
   const code = fn.toString();
 
   let promiseType = 'Promise.?unknown?';
-  if (code.includes('Promise.all')) {
-    promiseType = 'Promise.all()';
-  } else if (code.includes('Promise.allSettled')) {
+  if (code.includes('Promise.allSettled')) {
     promiseType = 'Promise.allSettled()';
+  } else if (code.includes('Promise.all')) {
+    promiseType = 'Promise.all()';
   } else if (code.includes('Promise.any')) {
     promiseType = 'Promise.any()';
   } else if (code.includes('Promise.race')) {
